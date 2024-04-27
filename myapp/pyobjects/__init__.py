3ce331292f472d3b9b866e6a028c33f83582012d
Elias Mueller <elias.mr1@gmail.com>
2024-04-27 16:00:35 +0200

    Add clipboard pyobject

--- myapp/pyobjects/__init__.py
+++ myapp/pyobjects/__init__.py
@@ -13,5 +13,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+from .clipboard import MyClipboardPyObject
 from .example_singleton import SingletonPyObject
