03be549115cd265333f6d3fb9417055fe8592609
Elias Mueller <elias.mr1@gmail.com>
2025-10-17 10:02:38 +0200

    chore: use SPDX license headers

--- src/framelesswindow/linux/event.py
+++ src/framelesswindow/linux/event.py
@@ -1,21 +1,7 @@
-# Copyright
+# SPDX-FileCopyrightText: zhiyiYo
+# SPDX-FileCopyrightText: <Your Name>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-# Inspired and based on:
-#  - https://github.com/zhiyiYo/PyQt-Frameless-Window
-#  - https://gitee.com/Virace/pyside6-qml-frameless-window/tree/main
-
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from typing import Optional
 
